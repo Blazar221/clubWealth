ab4e7caaf96ac05b0d2493056e271609e561dd66
Blazar <805781038@qq.com>
2023-03-03 14:25:46 -0800

    add bg for nav link

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -36,14 +36,12 @@ const Sidebar = () => {
             <div className='text-light text-decoration-none p-2 d-flex gap-2 fs-3 align-items-center position-relative'
                 onClick={toggle}>
                 <p className={`m-0 cw-sidebar-logo-link cw-sidebar-logo-link-${isExpand ? "expand" : "collapse"}`}>Club Wealth</p>
-                {/* <div className={`cw-sidebar-logo-icon cw-sidebar-logo-icon-${isExpand ? "expand" : "collapse"}`}> */}
-                    <Icon className={`cw-sidebar-logo-icon cw-sidebar-logo-icon-${isExpand ? "expand" : "collapse"}`} icon="material-symbols:menu-rounded"></Icon>
-                {/* </div> */}
+                <Icon className={`cw-sidebar-logo-icon cw-sidebar-logo-icon-${isExpand ? "expand" : "collapse"}`} icon="material-symbols:menu-rounded"></Icon>
             </div>
             {
                 menuItem.map(data => {
-                    return <NavLink to={data.link} className="text-light text-decoration-none p-2 d-flex gap-2 align-items-center">
-                        <Icon className="fs-4" style={{ minWidth: "2.5rem" }} icon={data.icon}></Icon>
+                    return <NavLink to={data.link} className="cw-sidebar-nav text-decoration-none p-2 d-flex gap-2 align-items-center">
+                        <Icon className="fs-4" style={{ minWidth: "2.5rem", zIndex: 3 }} icon={data.icon}></Icon>
                         <p className={`m-0 cw-sidebar-nav-link cw-sidebar-nav-link-${isExpand ? "expand" : "collapse"}`}>{data.title}</p>
                     </NavLink>
                 })
