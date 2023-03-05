8195f918d894ece2e8fd181d123f7c96b8b902d5
Blazar <805781038@qq.com>
2023-03-04 23:30:08 -0800

    pagination nav improve

--- src/components/catCard.jsx
+++ src/components/catCard.jsx
@@ -10,17 +10,25 @@ const CatCard = ({ url, catId }) => {
     const dislike = (itemId) => {
         dispatch(removeCat(itemId))
     }
-
+    debugger
     return (
         <div className="card cw-card p-0 m-1" style={{ width: "320px" }}>
-            <div className='cw-card-dislike' onClick={() => dislike(catId)}>
-                <Icon icon="mdi:heart-off-outline" className='text-light fs-2' />
-            </div>
-            <img src={url} className="card-img-top" alt={catId}
-                style={{ minWidth: "100%", maxWidth: "100%", minHeight: "20rem", maxHeight: "20rem" }}
-            />
+            {
+                url && <div className='cw-card-dislike' onClick={() => dislike(catId)}>
+                    <Icon icon="mdi:heart-off-outline" className='text-light fs-2' />
+                </div>
+            }
+            {
+                url ?
+                    <img src={url} className="card-img-top" alt={catId}
+                        style={{ minWidth: "100%", maxWidth: "100%", minHeight: "20rem", maxHeight: "20rem" }}
+                    /> :
+                    <span style={{ backgroundColor: "#C4CEE3", minWidth: "100%", maxWidth: "100%", minHeight: "20rem", maxHeight: "20rem" }}></span>
+            }
             <div className="card-body">
-                <h5 className="card-title">{catId}</h5>
+                <h5 className="card-title placeholder-glow">
+                    {url ? <span>{catId}</span> : <span class="placeholder w-100" style={{ height: "1rem" }}></span>}
+                </h5>
             </div>
         </div>
     );
--- src/pages/pageCat.jsx
+++ src/pages/pageCat.jsx
@@ -10,7 +10,22 @@ const PageCat = () => {
     const status = useSelector((state) => state.cat.status);
     const error = useSelector((state) => state.cat.error);
     const pages = useSelector((state) => state.cat.pages);
-    const pagesArray = Array.from({ length: pages }, (_, index) => index);
+    debugger
+    let pagesArray;
+
+    switch (pages) {
+        case 1:
+            pagesArray = [0]
+            break
+        case 2:
+            pagesArray = [0, 1]
+            break
+        case 3:
+            pagesArray = [0, 1, 2]
+            break
+        default:
+            pagesArray = curPage == 0 ? [0, 1, 2] : curPage == pages - 1 ? [curPage - 2, curPage - 1, curPage] : [curPage - 1, curPage, curPage + 1]
+    }
 
     useEffect(() => {
         if (status === 'idle') {
@@ -23,7 +38,30 @@ const PageCat = () => {
     }
 
     if (status === 'loading') {
-        return <div>Loading...</div>;
+        return <div>
+            <div className='container d-flex flex-wrap align-items-center justify-content-around'>
+                <div className='row w-100 d-flex justify-content-evenly'>
+                    <div className='col-12 col-lg-4 d-flex justify-content-center'>
+                        <CatCard />
+                    </div>
+                    <div className='col-12 col-lg-4 d-flex justify-content-center'>
+                        <CatCard />
+                    </div>
+                    <div className='col-12 col-lg-4 d-flex justify-content-center'>
+                        <CatCard />
+                    </div>
+                </div>
+            </div>
+            <nav className='w-25 mx-auto my-3'>
+                <ul class="pagination">
+                    <li class="page-item" ><p style={{ cursor: "pointer" }} class="page-link m-0">First</p></li>
+                    <li class="page-item" >
+                        <p style={{ cursor: "pointer" }} class="page-link m-0">1</p>
+                    </li>
+                    <li class="page-item" ><p style={{ cursor: "pointer" }} class="page-link m-0">Last</p></li>
+                </ul >
+            </nav >
+        </div >;
     }
 
     if (status === 'failed') {
@@ -44,19 +82,19 @@ const PageCat = () => {
                 </div>
                 <nav className='w-25 mx-auto my-3'>
                     <ul class="pagination">
-                        <li class="page-item" onClick={() => changePage(Math.max(0, curPage - 1))}><p class="page-link m-0">Previous</p></li>
+                        <li class="page-item" onClick={() => changePage(0)}><p style={{ cursor: "pointer" }} class="page-link m-0">First</p></li>
                         {
                             pagesArray.map(pageIdx => {
                                 return <li class="page-item" onClick={() => changePage(pageIdx)}>
-                                    <p class={`page-link m-0 ${pageIdx == curPage ? "text-light" : ""}`}>{pageIdx + 1}</p>
+                                    <p style={{ cursor: "pointer" }} class={`page-link m-0 ${pageIdx == curPage ? "text-light" : ""}`}>{pageIdx + 1}</p>
                                 </li>
                             })
                         }
-                        <li class="page-item" onClick={() => changePage(Math.min(pages - 1, curPage + 1))}><p class="page-link m-0">Next</p></li>
+                        <li class="page-item" onClick={() => changePage(pages - 1)}><p style={{ cursor: "pointer" }} class="page-link m-0">Last</p></li>
                     </ul>
                 </nav>
-            </div> : <p>no data</p>}
-        </div>
+            </div > : <p>no data</p>}
+        </div >
     );
 }
 
